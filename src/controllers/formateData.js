88ac081d8f2319b6de5f8edce7eb51f1980c119f
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-03-09 15:51:48 +0530

    formateData controller added

--- src/controllers/formateData.js
+++ src/controllers/formateData.js
@@ -1,5 +1,7 @@
 import { stats } from "../model/stats.js"
 import createSVG from "../view/createSVG.js"
+import { whoamiSVG } from "../view/whoamiSVG.js";
+import { statsSVG } from "../view/statsSVG.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,11 +10,15 @@ const token = process.env.TOKEN;
 
 const formateRequest =  async (req)=> {
     let username = req.name
+    console.log(req)
     
-    const s = await stats("vskvj3", token);
-    console.log(s)
+    const st = await stats(username, token);
+    const statSVG = await statsSVG(st, 140, 20, 6400, 1600)
+    
 
-    return createSVG(req, 640, 500)
+    const whoami = whoamiSVG(username, 80, 20, 1600, 1600);
+
+    return createSVG(req, 640, 500, whoami, statSVG)
 }
 
 const renderStats = ()=> {
