06f04db203f2e9a4ba23b07b7ace7022d0488be4
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-03-09 15:55:51 +0530

    support for components

--- src/app.js
+++ src/app.js
@@ -1,30 +1,29 @@
 import  Express  from "express";
-import createSVG  from "./view/createSVG.js";
 import dotenv from "dotenv";
+
+import { formateRequest } from "./controllers/formateData.js";
+
+
 dotenv.config()
 
 // testing
 const token = process.env.TOKEN;
-import topThreeRepo from "./model/fetchData.js";
 
 
 const app = Express()
 const PORT = process.env.PORT;
 
-const resp = await topThreeRepo("vskvj3"); 
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res)=> { 
     res.send({status:"working"});
 }) 
 
-app.get('/svg/', (req, res)=> {
+app.get('/svg/', async (req, res)=> {
 
     res.send(
-        createSVG(req.query, 640, 500)
+        await formateRequest(req.query)
     ); 
 })
-
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port: http://localhost:${PORT}`)
